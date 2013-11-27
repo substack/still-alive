81f36fc140f2ec822addbca4ec0b2065330e4dc7
James Halliday <mail@substack.net>
2013-11-27 04:17:52 -0800

    much more interesting synth

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ var music = [
     ['E5','F4'],['E5','F4'],
     ['A4','D4'],['A4','D4'],
     
-    
 ];
 var bps = 120 / 60;
 
@@ -40,11 +39,20 @@ module.exports = function (t) {
     var note = music[Math.floor(t * bps * 4) % music.length];
     var sum = 0;
     for (var i = 0, l = note.length; i < l; i++) {
-        sum += sin(notes[note[i]]);
+        var x = notes[note[i]];
+        sum += 0
+            + sin(x / 2) * 0.2
+            + sin(x / 2 + 2) * 0.2
+            + sin(x) * 0.3 + sin(x + 8) * 0.15
+            + sin(x * 2 + sin(400 * x)) * 0.15
+            + sin(x * 2 + 4) * 0.1
+            + sin(x * 3) * 0.25
+            + sin(x * 4 + sin(100 * x)) * 0.1
+        ;
     }
     return l ? sum / Math.sqrt(l) : 0;
     
     function sin (x) {
-        return Math.sin(2 * Math.PI * t * x);
+        return Math.sin(2 * Math.PI * (t % 4) * x);
     }
 };
